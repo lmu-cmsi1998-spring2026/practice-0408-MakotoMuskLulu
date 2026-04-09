79b76943cabfa1a5f86dfd057aba185249dcb373
Musk <musk.1109@gmail.com>
2026-04-08 18:12:59 -0700

    Fix typo in days_in_month

--- days_in_month.py
+++ days_in_month.py
@@ -3,7 +3,7 @@ from is_leap_year import is_leap_year
 
 def days_in_month(year: int, month: int) -> int:
     if month == 2:
-        return 29 if isleapyer(year) else 28
+        return 29 if is_leap_year(year) else 28
     elif month in (1,3,5,7,8,10,12):
         return 31
     elif month in (4,6,9,11):
